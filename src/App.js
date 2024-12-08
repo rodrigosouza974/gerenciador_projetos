f499d038643359a803549e27e918f19975bc5d12
Rodrigo souza <124813706+rodrigosouza974@users.noreply.github.com>
2024-12-07 23:52:03 -0300

    footer e navbar

--- src/App.js
+++ src/App.js
@@ -4,26 +4,24 @@ import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
 import Container from './components/layout/Container';
-
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Projetcs from './components/pages/Projects';
 
 function App() {
   return ( 
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo Projeto</Link>
-      </div>
+      <Navbar/>
       <Container customClass ="min-height">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/company" element={<Company />} />
           <Route path="/newproject" element={<NewProject />} />
+          <Route path="/projetcs" element={<Projetcs />} />
         </Routes>
       </Container>
-      <p>Footer</p>
+      <Footer/> 
     </Router>
   );
 }
---
+++ src/components/pages/Projects.js
@@ -0,0 +1,7 @@
+//import styles from './Footer.module.css'
+
+function Projects() {
+  return <h1>projetos</h1>
+}
+
+export default Projects
